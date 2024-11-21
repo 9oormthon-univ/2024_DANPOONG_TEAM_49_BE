cbc81c58176ee3c78f3d1fde090371871ba295a9
JungHyunSeok <jhyunseok325@gmail.com>
2024-11-21 15:17:18 +0900

    [GOORM-2]-feat: Implementing the Kakao Login API

---
+++ src/main/java/com/goormthon3/team49/domain/user/presentation/UserInfoResponseDto.java
@@ -0,0 +1,38 @@
+package com.goormthon3.team49.domain.user.presentation;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class UserInfoResponseDto {
+
+    @JsonProperty("id")
+    private Long id; //카카오에서 사용자 고유 ID
+
+    @JsonProperty("kakao_account")
+    private KakaoAccount kakaoAccount;
+
+    @Getter
+    @NoArgsConstructor
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class KakaoAccount {
+
+        @JsonProperty("profile")
+        private Profile profile;
+
+        @Getter
+        @NoArgsConstructor
+        @JsonIgnoreProperties(ignoreUnknown = true)
+        public static class Profile {
+            @JsonProperty("nickname")
+            private String nickName; //닉네임
+
+            @JsonProperty("profile_image_url")
+            private String profileImageUrl; //프로필 사진 URL
+        }
+    }
+}
