4bc3a073e6b5a5f7a8eb49b578ad66ff5ba964fc
JungHyunSeok <jhyunseok325@gmail.com>
2024-11-23 18:38:45 +0900

    [GOORM-8]-feat: Email Verify Implemention with Redis

--- src/main/java/com/goormthon3/team49/domain/email/presentation/MailController.java
+++ src/main/java/com/goormthon3/team49/domain/email/presentation/MailController.java
@@ -3,9 +3,9 @@ package com.goormthon3.team49.domain.email.presentation;
 import com.goormthon3.team49.domain.email.application.MailService;
 import jakarta.mail.MessagingException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.io.UnsupportedEncodingException;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequiredArgsConstructor
@@ -14,11 +14,25 @@ public class MailController {
 
     private final MailService mailService;
 
-    @ResponseBody
-    @PostMapping("/email") // 이 부분은 각자 바꿔주시면 됩니다.
-    public String emailCheck(@RequestBody MailDto mailDTO) throws MessagingException, UnsupportedEncodingException {
-        String authCode = mailService.sendSimpleMessage(mailDTO.getEmail());
+    //이메일로 인증코드 발송
+    @PostMapping("/email")
+    public ResponseEntity<?> sendEmail(@RequestBody MailDto mailDto) throws MessagingException {
 
-        return authCode; // Response body에 값을 반환
+        mailService.sendVerificationEmail(mailDto.getEmail());
+
+        return ResponseEntity.ok("인증 코드가 이메일로 전송되었습니다.");
+    }
+
+    //인증코드 검증
+    @PostMapping("/email/verify")
+    public ResponseEntity<String> verifyCode(@RequestBody MailVerifyDto verifyDto) {
+
+        try {
+            mailService.verifyAuthCode(verifyDto.getEmail(), verifyDto.getAuthCode());
+
+            return ResponseEntity.ok("인증이 완료되었습니다.");
+        } catch (ResponseStatusException ex) {
+            return ResponseEntity.status(ex.getStatusCode()).body(ex.getReason());
+        }
     }
 }
