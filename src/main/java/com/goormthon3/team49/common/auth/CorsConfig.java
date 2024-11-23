235e518c9c293faa4a4ea923127d4a927b4738ac
sunny2you <ally1008@naver.com>
2024-11-23 18:44:02 +0900

    [GOORM-7]-fix: preflight 관련 추가

---
+++ src/main/java/com/goormthon3/team49/common/auth/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.goormthon3.team49.common.auth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true); // 쿠키나 인증 정보를 포함한 요청 허용
+        config.addAllowedOrigin("http://localhost:3000"); // 허용할 Origin
+        config.addAllowedHeader("*"); // 모든 헤더 허용
+        config.addAllowedMethod("*"); // 모든 HTTP 메서드 허용 (GET, POST, PUT, DELETE 등)
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
+
